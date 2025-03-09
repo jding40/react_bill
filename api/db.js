7a70f9958e914b1bc82302ef853a4661f757fe1e
Coca4017 <jding40@myseneca.ca>
2025-03-08 23:13:19 -0500

    set cors

--- api/db.js
+++ api/db.js
@@ -5,6 +5,16 @@ import data from "../server/data.json";
 const filePath = path.join(process.cwd(), "server", "data.json");
 
 export default function handler(req, res) {
+  // 设置 CORS 头
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // 处理 OPTIONS 请求
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method === "GET") {
     return res.status(200).json(data.ka);
   }
